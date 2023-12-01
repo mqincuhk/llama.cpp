5d5d60a1457ac79bbb5e5a18fe4eaa62a5736e47
mqin666 <44671490+mqincuhk@users.noreply.github.com>
2023-12-01 15:00:49 +0800

    update

--- examples/chat-finetune.sh
+++ examples/chat-finetune.sh
@@ -32,5 +32,5 @@ cd ..
 ./main -m ./models/7B/ggml-model-q4_0.gguf -c 512 -b 1024 -n 256 --keep 48 \
     --lora lora-open-llama-7b-reason-LATEST.bin \
     --repeat_penalty 1.0 --color -i \
-    -r "User:" -f prompts/chat-with-bob.txt 
+    -r "User:" -f prompts/chat-reason.txt 
 """
